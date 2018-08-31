1f9c92b01686855141e23190220dd2efe70600ed
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-08-31 18:20:27 +0800

    render html file

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from datetime import datetime
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
