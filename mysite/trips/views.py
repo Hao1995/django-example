66b99499b62cc958fe41317bbe5358deac768a8a
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2018-09-02 15:37:58 +0800

    template usage

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Post
 
 def hello_world(request):
     # return HttpResponse("Hello World!")
     return render(request, 'hello_world.html', {
         'current_time': str(datetime.now()),
+    })
+
+def home(request):
+    
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
     })
